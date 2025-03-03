d7be3828cdf5b01faba24ab09677f328d9a4bdfd
Caio <caio.jannuzzi@outlook.com>
2025-03-03 20:48:38 -0300

    add superuser and post table

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# admin.site.register(Post) # forma padrão de registrar um model no admin
+
+@admin.register(Post) # forma mais elegante de registrar um model no admin
+class Post(admin.ModelAdmin):
+    list_display = ('author','title', 'text', 'created_date', 'published_date') # exibe esses campos na listagem do admin
+    
