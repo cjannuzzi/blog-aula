91792e484b1c0474776cd414b1755ed895610461
Caio <caio.jannuzzi@outlook.com>
2025-03-05 21:13:01 -0300

    add exemples  of template and views

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def portao(request):
-    return HttpResponse('Você chegou ao portão da casa')
-
-def sala(request):
-    return HttpResponse('Você chegou à sala da casa, sente-se no sofá')
-
-def quarto(request):
-    return HttpResponse('Você chegou ao quarto da casa, vá dormir')
-
 def post_list(request):
     return render(request, 'blog/post_list.html', {}) # O template é blog/post_list.html que está na pasta templates/blog e o contexto é um dicionário vazio {}.
+
+def portao(request):
+    return render(request,'blog/porta.html', {}) 
+
+def sala(request):
+    return render(request,'blog/sala-simples.html', {}) 
+
+def quarto(request):
+    return render(request,'blog/quarto-simples.html', {})
+
+
